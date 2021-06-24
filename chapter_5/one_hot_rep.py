cfc324e9104ecb2eaf738caaef94b253e4283b06
Graham-CO <grwi2594@colorado.edu>
2021-06-24 12:26:56 -0600

    add some print() statements

--- chapter_5/one_hot_rep.py
+++ chapter_5/one_hot_rep.py
@@ -5,12 +5,14 @@
 import numpy as np
 
 softmax_outputs = np.array([[0.7, 0.1, 0.2],
-                            [0.1, 0.5, 0.4]
+                            [0.1, 0.5, 0.4],
                             [0.02, 0.9, 0.08]])
 
 class_targets = np.array([[1, 0, 0],
                          [0, 1, 0],
                          [0, 1, 0]])
+print(class_targets)
+print(len(class_targets.shape))
 
 # Probabilities for target vals -
 # only if categorical labels
@@ -25,6 +27,8 @@ elif len(class_targets.shape) == 2:
     correct_confidences = np.sum(
         softmax_outputs*class_targets,
         axis=1)
+
+print(correct_confidences)
 
 # Losses
 neg_log = -np.log(correct_confidences)
