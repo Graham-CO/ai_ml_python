757f6ba28da818b8791b4fbf629b31491ad5d256
Graham-CO <grwi2594@colorado.edu>
2021-06-24 12:29:34 -0600

    Template for a standard loss class

---
+++ chapter_5/loss_class.py
@@ -0,0 +1,20 @@
+## Categorical Cross-Entropy Loss Class
+# Graham Williams
+# grw400@gmail.com
+
+# Common loss class
+class Loss:
+
+    # Calculates the data and regularization losses
+    # given model output and ground truth values
+    def calculate(self, output, y):
+
+        # Calculate sample losses
+        sample_losses = self.forward(output, y)
+
+        # Calculate mean loss
+        data_loss = np.mean(sample_losses)
+
+        # Return loss
+        return data_loss
+
